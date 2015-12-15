d194dde0e78c07c74ad80e7cd2d777c13b593905
frandman <franjubrique@hotmail.com>
2015-12-15 12:58:24 +0100

    fixed delay bug

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 var map;
+
+/* Map Loader Function */
+
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 40.425884, lng: -3.68783},
     zoom: 14
   });
+	this.loadImgInfo();
 }
-
-
 
 /* Location Model */
 
@@ -24,10 +26,12 @@ function Loc (data) {
 
 /* types */
 
-var types ={'1':'Museum','2':'Building','3':'Square','4':'Restaurant'};
+var types ={'1':'Museum','2':'Building','3':'Square'};
+
 /* Data */
 
 locationsData =[
+
 	{'type': '1',
 	 'name': 'Torres de Colon',
 	 'latlng':{lat: -34.397, lng: 150.644}
@@ -37,13 +41,28 @@ locationsData =[
 	 'name': 'Museo del Prado',
 	 'latlng':{lat: -34.397, lng: 150.644}
 	},
+
+	{'type': '3',
+	 'name': 'Puerta del Sol',
+	 'latlng':{lat: -3.70, lng: 40.41}
+	},
+
+	{'type': '3',
+	 'name': 'Faro Moncloa',
+	 'latlng':{lat: -3.70, lng: 40.41}
+	},
+
+	{'type': '3',
+	 'name': 'Parque de el Retiro',
+	 'latlng':{lat: -3.70, lng: 40.41}
+	},
 ];
 
 /* Octopus */
 
 var ViewModel = function () {
+
 	self = this;
-	var request = {};
 	self.locations = ko.observableArray();
 
 	for (var i =0; i < locationsData.length; i++){
@@ -58,30 +77,38 @@ var ViewModel = function () {
 
 	self.currentLoc = ko.observable(self.locations()[0]);
 
-	updateInfoWindow = function(loc){
-		self.currentLoc(this);
-		request.query = this.title();
-		service(this, request);
+	loadImgInfo = function(){
+		var loc;
+		var request = {};
+		loc = self.locations()[0]
+		request.query = loc.title();
+		service(loc, request);
 	}
 
+	updateInfoWindow = function(loc){
+		var request = {};
+		self.currentLoc(loc);
+		request.query = loc.title();
+		service(loc, request);
+	}
 };
 
 
-wikiInfoRequest = function (Loc){
-	_url = "https://es.wikipedia.org/w/api.php?action=opensearch&search="+Loc.title()+"&format=json&callback=wikiCallback";
+wikiInfoRequest = function (loc){
+	_url = "https://es.wikipedia.org/w/api.php?action=opensearch&search="+loc.title()+"&format=json&callback=wikiCallback";
 	$.ajax({
   		url: _url,
   		dataType: "jsonp"
 		}).done(function(res){
-			Loc.description(res[2][0]);
-			Loc.urlinfo= res[3][0];
+			loc.description(res[2][0]);
+			loc.urlinfo= res[3][0];
 	});
 }
 
-function service(Loc,request){
+function service(loc,request){
 	var service = new google.maps.places.PlacesService(map);
 	service.textSearch(request, function(res){
-		Loc.img_url = res[0].photos[0].getUrl({'maxWidth': 400, 'maxHeight': 1000});
+		loc.img_url(res[0].photos[0].getUrl({'maxWidth': 400, 'maxHeight': 400}));
 	});
 }
 
