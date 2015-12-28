59b97283a3ebf0aa2bcc0016a6ac0798745aef4c
frandman <franjubrique@hotmail.com>
2015-12-29 00:20:05 +0100

    Functions Moved into ViewModel

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ function Loc (data) {
     self.urlinfo = ko.observable();
     self.img_small = ko.observable();
     self.img_large = ko.observable();
-    self.map = map;
+    self.map = ko.observable(map);
     self.contentString = ko.observable();
 }
 
@@ -76,7 +76,6 @@ locationsData =[
 /* Octopus */
 
 var ViewModel = function () {
-
     var self = this;
     self.query = ko.observable("");
     self.locations = ko.observableArray();
@@ -102,8 +101,96 @@ var ViewModel = function () {
     	toggleAnimation(loc.marker);
     }
 
-    self.search = ko.computed(function() {
-    	return ko.utils.arrayFilter(self.locations(), function(location) {
+	/* Load wikipedia Info */
+
+	function wikiInfoRequest(loc) {
+	    var _url = "https://es.wikipedia.org/w/api.php?action=opensearch&search="+loc.title()+"&format=json&callback=wikiCallback";
+	    var wikiRequestTimeout = setTimeout( function() {
+	    	alert("Wikipedia Error");
+	    }, 8000);
+	    $.ajax({
+	    	url: _url,
+	    	dataType: "jsonp",
+	    	success: function(res){
+	    		loc.description(res[2][0]);
+	    		loc.urlinfo(res[3][0]);
+	    	    loadLocImg(loc, loc.request());
+	    	    clearTimeout(wikiRequestTimeout);
+	    	}
+	    });
+	}
+
+	/* Handle markers visibility depending on search */
+
+    var setVisibilty = function(filteredLocations) {
+	    for (var i = 0; i < markers.length; i++) {
+	        for (var j = 0; j < filteredLocations.length; j++) {
+	            var filteredMark = filteredLocations[j];
+	            if (filteredMark.title() === markers[i].title) {
+	                markers[i].setVisible(true);
+	                break;
+
+	            } else {
+	                markers[i].setVisible(false);
+	            }
+	        }
+	    }
+	}
+
+	/* Add animation on markers*/
+
+	function toggleAnimation(marker) {
+	  if (marker.getAnimation() !== null) {
+	    marker.setAnimation(null);
+	  } else {
+	    marker.setAnimation(google.maps.Animation.DROP);
+	  }
+	}
+
+	/* Load images from Google Maps API and generates markers */
+
+	function loadLocImg(loc,request) {
+		var service = new google.maps.places.PlacesService(map);
+		service.textSearch(request, function(res){
+			if (res){
+				var url_small = res[0].photos[0].getUrl({'maxWidth': 200, 'maxHeight': 200});
+				var url_large = res[0].photos[0].getUrl({'maxWidth': 400, 'maxHeight': 400});
+				loc.img_small(url_small);
+				loc.img_large(url_large);
+				loc.contentString('<div class="container infowindow">'+
+										'<div class="row iw-title">'+
+								       		'<h1>'+loc.title()+'</h1>'+
+								       	'</div>'+
+								       	'<div class="row">'+
+								        	'<img class="img-responsive" src="'+url_small+'">'+
+								        '</div>'+
+								        '<div class="row iw-description">'+
+								        	'<p>'+loc.description()+'</p>'+
+								        '</div>'+
+								   '</div>');
+	        }
+	        else {
+	        	loc.img_small('img/error.png');
+	        	loc.contentString('<h1>Error retrieving location info</h1>');
+	        }
+	        loc.infowindow = new google.maps.InfoWindow({
+	        	content: loc.contentString()
+	        });
+	        loc.marker = new google.maps.Marker({
+	        	position: loc.latlng(),
+	            map: map,
+	    	    title: loc.title(),
+	    	    animation: google.maps.Animation.DROP
+	        });
+	        loc.marker.addListener("click", function(){
+	            loc.infowindow.open(map, this);
+	            toggleAnimation(this);
+	        });
+	        markers.push(loc.marker);
+	    });
+	}
+	self.search = ko.computed(function() {
+        return ko.utils.arrayFilter(self.locations(), function(location) {
     		return location.title().toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
     	});
     });
@@ -114,95 +201,6 @@ var ViewModel = function () {
 }
 
 
-
-/* Load wikipedia Info */
-
-function wikiInfoRequest(loc) {
-    var _url = "https://es.wikipedia.org/w/api.php?action=opensearch&search="+loc.title()+"&format=json&callback=wikiCallback";
-    var wikiRequestTimeout = setTimeout( function() {
-    	alert("Wikipedia Error");
-    }, 8000);
-    $.ajax({
-    	url: _url,
-    	dataType: "jsonp",
-    	success: function(res){
-    		loc.description(res[2][0]);
-    		loc.urlinfo(res[3][0]);
-    	    loadLocImg(loc, loc.request());
-    	    clearTimeout(wikiRequestTimeout);
-    	}
-    });
-}
-
-/* Load images from Google Maps API and generates markers */
-
-function loadLocImg(loc,request) {
-	var service = new google.maps.places.PlacesService(map);
-	service.textSearch(request, function(res){
-		if (res){
-			var url_small = res[0].photos[0].getUrl({'maxWidth': 200, 'maxHeight': 200});
-			var url_large = res[0].photos[0].getUrl({'maxWidth': 400, 'maxHeight': 400});
-			loc.img_small(url_small);
-			loc.img_large(url_large);
-			loc.contentString('<div class="container infowindow">'+
-									'<div class="row iw-title">'+
-							       		'<h1>'+loc.title()+'</h1>'+
-							       	'</div>'+
-							       	'<div class="row">'+
-							        	'<img class="img-responsive" src="'+url_small+'">'+
-							        '</div>'+
-							        '<div class="row iw-description">'+
-							        	'<p>'+loc.description()+'</p>'+
-							        '</div>'+
-							   '</div>');
-        }
-        else {
-        	loc.img_small('img/error.png');
-        	loc.contentString('<h1>Error retrieving location info</h1>');
-        }
-        loc.infowindow = new google.maps.InfoWindow({
-        	content: loc.contentString()
-        });
-        loc.marker = new google.maps.Marker({
-        	position: loc.latlng(),
-            map: map,
-    	    title: loc.title(),
-    	    animation: google.maps.Animation.DROP
-        });
-        loc.marker.addListener("click", function(){
-            loc.infowindow.open(map, this);
-            toggleAnimation(this);
-        });
-        markers.push(loc.marker);
-    });
-}
-
-/* Handle markers visibility depending on search */
-
-var setVisibilty = function(filteredLocations) {
-    for (var i = 0; i < markers.length; i++) {
-        for (var j = 0; j < filteredLocations.length; j++) {
-            var filteredMark = filteredLocations[j];
-            if (filteredMark.title() === markers[i].title) {
-                markers[i].setVisible(true);
-                break;
-
-            } else {
-                markers[i].setVisible(false);
-            }
-        }
-    }
-}
-
-/* Add animation on markers*/
-
-function toggleAnimation(marker) {
-  if (marker.getAnimation() !== null) {
-    marker.setAnimation(null);
-  } else {
-    marker.setAnimation(google.maps.Animation.DROP);
-  }
-}
 
 
 
