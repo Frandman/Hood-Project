19fb22aff079a23ba143d05cf0a701f5de989a37
frandman <franjubrique@hotmail.com>
2015-12-29 08:10:54 +0100

    linting code

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ function Loc (data) {
     self.country = ko.observable("Spain");
     self.type = ko.observable(types[data.type]);
     self.request = ko.computed(function(){
-    	return {query: data.name}
+        return {query: data.name};
     });
     self.description = ko.observable();
     self.urlinfo = ko.observable();
@@ -70,7 +70,7 @@ locationsData =[
     {'type': '3',
     'name': 'Plaza de Castilla',
     'latlng':{lat: 40.4657783, lng: -3.6908562}
-    },
+    }
 ];
 
 /* Octopus */
@@ -84,121 +84,121 @@ var ViewModel = function () {
         wikiInfoRequest(self.locations()[i]);
     }
     self.types = ko.observableArray();
-    for (var i = 1; i <= Object.keys(types).length; i++) {
-        self.types.push(types[i]);
+    for (var j = 1; j <= Object.keys(types).length; j++) {
+        self.types.push(types[j]);
     }
     self.currentLoc = ko.observable(self.locations()[0]);
 
-	/* Update information and open infowindow */
+    /* Update information and open infowindow */
 
     self.updateInfoWindow = function(loc){
-    	self.currentLoc(loc);
-    	if (infowindow){
-    		infowindow.close();
-    	}
-    	infowindow.setContent(loc.contentString());
-    	infowindow.open(map, loc.marker);
-    	toggleAnimation(loc.marker);
+        self.currentLoc(loc);
+        if (infowindow){
+            infowindow.close();
+        }
+        infowindow.setContent(loc.contentString());
+        infowindow.open(map, loc.marker);
+        toggleAnimation(loc.marker);
+    };
+
+    /* Load wikipedia Info */
+
+    function wikiInfoRequest(loc) {
+        var _url = "https://es.wikipedia.org/w/api.php?action=opensearch&search="+loc.title()+"&format=json&callback=wikiCallback";
+        var wikiRequestTimeout = setTimeout( function() {
+            alert("Wikipedia Error");
+        }, 8000);
+        $.ajax({
+            url: _url,
+            dataType: "jsonp",
+            success: function(res){
+                loc.description(res[2][0]);
+                loc.urlinfo(res[3][0]);
+                loadLocImg(loc, loc.request());
+                clearTimeout(wikiRequestTimeout);
+            }
+        });
     }
 
-	/* Load wikipedia Info */
-
-	function wikiInfoRequest(loc) {
-	    var _url = "https://es.wikipedia.org/w/api.php?action=opensearch&search="+loc.title()+"&format=json&callback=wikiCallback";
-	    var wikiRequestTimeout = setTimeout( function() {
-	    	alert("Wikipedia Error");
-	    }, 8000);
-	    $.ajax({
-	    	url: _url,
-	    	dataType: "jsonp",
-	    	success: function(res){
-	    		loc.description(res[2][0]);
-	    		loc.urlinfo(res[3][0]);
-	    	    loadLocImg(loc, loc.request());
-	    	    clearTimeout(wikiRequestTimeout);
-	    	}
-	    });
-	}
-
-	/* Handle markers visibility depending on search */
+    /* Handle markers visibility depending on search */
 
     var setVisibilty = function(filteredLocations) {
-	    for (var i = 0; i < markers.length; i++) {
-	        for (var j = 0; j < filteredLocations.length; j++) {
-	            var filteredMark = filteredLocations[j];
-	            if (filteredMark.title() === markers[i].title) {
-	                markers[i].setVisible(true);
-	                break;
+        for (var i = 0; i < markers.length; i++) {
+            for (var j = 0; j < filteredLocations.length; j++) {
+                var filteredMark = filteredLocations[j];
+                if (filteredMark.title() === markers[i].title) {
+                    markers[i].setVisible(true);
+                    break;
 
-	            } else {
-	                markers[i].setVisible(false);
-	            }
-	        }
-	    }
-	}
+                } else {
+                    markers[i].setVisible(false);
+                }
+            }
+        }
+    };
 
-	/* Add animation on markers*/
+    /* Add animation on markers*/
 
-	function toggleAnimation(marker) {
-	  if (marker.getAnimation() !== null) {
-	    marker.setAnimation(null);
-	  } else {
-	    marker.setAnimation(google.maps.Animation.DROP);
-	  }
-	}
+    function toggleAnimation(marker) {
+      if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+      } else {
+        marker.setAnimation(google.maps.Animation.DROP);
+      }
+    }
 
-	/* Load images from Google Maps API and generates markers */
+    /* Load images from Google Maps API and generates markers */
 
-	function loadLocImg(loc,request) {
-		var service = new google.maps.places.PlacesService(map);
-		service.textSearch(request, function(res){
-			if (res){
-				var url_small = res[0].photos[0].getUrl({'maxWidth': 200, 'maxHeight': 200});
-				var url_large = res[0].photos[0].getUrl({'maxWidth': 400, 'maxHeight': 400});
-				loc.img_small(url_small);
-				loc.img_large(url_large);
-				loc.contentString('<div class="container infowindow">'+
-										'<div class="row iw-title">'+
-								       		'<h1>'+loc.title()+'</h1>'+
-								       	'</div>'+
-								       	'<div class="row">'+
-								        	'<img class="img-responsive" src="'+url_small+'">'+
-								        '</div>'+
-								        '<div class="row iw-description">'+
-								        	'<p>'+loc.description()+'</p>'+
-								        '</div>'+
-								   '</div>');
-	        }
-	        else {
-	        	loc.img_small('img/error.png');
-	        	loc.contentString('<h1>Error retrieving location info</h1>');
-	        }
-	        loc.infowindow = new google.maps.InfoWindow({
-	        	content: loc.contentString()
-	        });
-	        loc.marker = new google.maps.Marker({
-	        	position: loc.latlng(),
-	            map: map,
-	    	    title: loc.title(),
-	    	    animation: google.maps.Animation.DROP
-	        });
-	        loc.marker.addListener("click", function(){
-	            loc.infowindow.open(map, this);
-	            toggleAnimation(this);
-	        });
-	        markers.push(loc.marker);
-	    });
-	}
-	self.search = ko.computed(function() {
+    function loadLocImg(loc,request) {
+        var service = new google.maps.places.PlacesService(map);
+        service.textSearch(request, function(res){
+            if (res){
+                var url_small = res[0].photos[0].getUrl({'maxWidth': 200, 'maxHeight': 200});
+                var url_large = res[0].photos[0].getUrl({'maxWidth': 400, 'maxHeight': 400});
+                loc.img_small(url_small);
+                loc.img_large(url_large);
+                loc.contentString('<div class="container infowindow">'+
+                                        '<div class="row iw-title">'+
+                                            '<h1>'+loc.title()+'</h1>'+
+                                        '</div>'+
+                                        '<div class="row">'+
+                                            '<img class="img-responsive" src="'+url_small+'">'+
+                                        '</div>'+
+                                        '<div class="row iw-description">'+
+                                            '<p>'+loc.description()+'</p>'+
+                                        '</div>'+
+                                   '</div>');
+            }
+            else {
+                loc.img_small('img/error.png');
+                loc.contentString('<h1>Error retrieving location info</h1>');
+            }
+            loc.infowindow = new google.maps.InfoWindow({
+                content: loc.contentString()
+            });
+            loc.marker = new google.maps.Marker({
+                position: loc.latlng(),
+                map: map,
+                title: loc.title(),
+                animation: google.maps.Animation.DROP
+            });
+            loc.marker.addListener("click", function(){
+                loc.infowindow.open(map, this);
+                toggleAnimation(this);
+            });
+            markers.push(loc.marker);
+        });
+    }
+    self.search = ko.computed(function() {
         return ko.utils.arrayFilter(self.locations(), function(location) {
-    		return location.title().toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
-    	});
+            return location.title().toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
+        });
     });
     self.search.subscribe( function() {
-    	setVisibilty(self.search());
+        setVisibilty(self.search());
     });
 
-}
+};
 
 
 
